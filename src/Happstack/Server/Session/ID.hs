9626bf687e08b0dd0c85b1f5bb53ab80cd95f9b1
Dag Odenhall <dag.odenhall@gmail.com>
2012-04-06 22:00:03 +0200

    safecopy instance for sessionid

--- src/Happstack/Server/Session/ID.hs
+++ src/Happstack/Server/Session/ID.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Happstack.Server.Session.ID where
+module Happstack.Server.Session.ID
+  ( SessionID
+  , getSessionID
+  , getSessionID'
+  ) where
 
 import Control.Applicative (Alternative, optional)
 import Control.Monad.Trans (MonadIO, liftIO)
-import Data.UUID           (UUID, toString, fromString)
+import Data.SafeCopy       (SafeCopy(putCopy, getCopy), contain, safePut, safeGet)
+import Data.UUID           (UUID, toString, fromString, toWords, fromWords)
 import Happstack.Server    (CookieLife(MaxAge), FilterMonad, Response, HasRqData, mkCookie, addCookie, lookCookieValue)
 import System.Random       (randomIO)
 
 newtype SessionID = SessionID UUID
+
+instance SafeCopy SessionID where
+  putCopy (SessionID uuid) = contain . safePut . toWords $ uuid
+  getCopy                  = contain $ do (a,b,c,d) <- safeGet
+                                          return $ SessionID $ fromWords a b c d
 
 getSessionID :: ( Alternative m
                 , FilterMonad Response m
